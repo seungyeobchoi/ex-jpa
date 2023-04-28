8380ccc68deeb04e7a5f02e8913c78a90b7c4b1c
seungyeobchoi <98461038+seungyeobchoi@users.noreply.github.com>
2023-04-28 21:21:07 +0900

    7th commit : add
    jpa sql how to use JPQL

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -21,13 +22,17 @@ public class JpaMain {
             em.persist(member);
             */
 
-            Member findMember = em.find(Member.class, 1L);
-            /*System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());*/
+            /*Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
 
-            findMember.setName("HelloJPA");
+            findMember.setName("HelloJPA");*/
 
-
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
