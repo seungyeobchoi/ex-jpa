8af1ee1eaaea6e56fdca47e356085c3f292c3eb6
seungyeobchoi <98461038+seungyeobchoi@users.noreply.github.com>
2023-04-28 19:50:22 +0900

    1th commit : add
    jpa 기초 작성

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,18 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager entityManager = emf.createEntityManager();
+        //이 위치에 코드 작성
+        entityManager.close();
+
+        emf.close();
+
+    }
+}
